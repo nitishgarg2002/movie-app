8889f7c184a83797d1ba22dcb80058de9ab81e11
nitishgarg2002 <nitishgarg2002@gmail.com>
2021-04-24 18:10:08 +0530

    redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,12 +6,14 @@
 //     type: 'DECREASE_COUNT
 // }
 
-import movies from "../reducers";
+
 
 export const ADD_MOVIES = 'ADD_MOVIES';
 export const ADD_TO_FAVOURITE = 'ADD_TO_FAVOURITE';
 export const REMOVE_FROM_FAVOURITES = 'REMOVE_FROM_FAVOURITES';
 export const SET_SHOW_FAVOURITES = 'SET_SHOW_FAVOURITES';
+export const ADD_SEARCH_RESULT = 'ADD_SEARCH_RESULT';
+export const ADD_MOVIE_TO_LIST = 'ADD_MOVIE_TO_LIST';
 export function addMovies (movies) {
     return {
         type: ADD_MOVIES,
@@ -36,14 +38,31 @@ export function setShowFavourites (val) {
         val
     }
 }
-const initialSearchState = {result: {}}
-export function search (state = initialSearchState,action) {
-    return state;
-}
 
-export default function rootReducer (state,action) {
+export function addMovieToList(movie) {
     return {
-        movies: movies(state,action),
-        search: search(state,action)
-    }
+      type: ADD_MOVIE_TO_LIST,
+      movie,
+    };
+  }
+
+
+export function handleMovieSearch(movie) {
+    return function (dispatch) {
+      const url = `http://www.omdbapi.com/?apikey=d50840c0&t=${movie}`;
+      fetch(url)
+        .then((response) => response.json())
+        .then((movie) => {
+          console.log('movie', movie);
+          // dispatch action to save search results in store
+          dispatch(addMovieSearchResult(movie));
+        });
+    };
+  }
+
+export function addMovieSearchResult (movie)  {
+    return {
+        type: ADD_SEARCH_RESULT,
+        movie
+    };
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
-import {combineReducers} from 'redux';
 
-import {ADD_MOVIES,ADD_TO_FAVOURITE,REMOVE_FROM_FAVOURITES,SET_SHOW_FAVOURITES} from '../actions/index';
+import {combineReducers} from 'redux';
+import {ADD_MOVIES,ADD_TO_FAVOURITE,ADD_MOVIE_TO_LIST,REMOVE_FROM_FAVOURITES,SET_SHOW_FAVOURITES,ADD_SEARCH_RESULT} from '../actions';
+
 const initialMovieState = {
     list: [],
     favourites: [],
@@ -37,20 +38,44 @@ export  function movies ( state = initialMovieState, action ) {
              return {
                  ...state,
                  showFavourites: action.val
-             }     
+             }
+             case ADD_MOVIE_TO_LIST:
+                return {
+                  ...state,
+                  list: [action.movie, ...state.list],
+                };
         default:
             return state;
         
     }
 }
-const initialSearchState = {result: {}}
-export function search (state = initialSearchState,action) {
-    return state;
+const initialSearchState = {
+    result: {},
+    showSearchResults: false
 }
+export function search (state = initialSearchState,action) {
+    switch(action.type) {
+        case ADD_SEARCH_RESULT:
+            return {
+                ...state,
+                result: action.movie,
+                showSearchResults: true
+            }
+            case ADD_MOVIE_TO_LIST:
+      return {
+        ...state,
+        showSearchResults: false,
+      };
+      
+        default:
+            return state;
+        
+    }
+};
 const initialRootState = {
     movies: initialMovieState,
     search: initialSearchState
-}
+};
 // export default function rootReducer (state=initialRootState,action) {
 //     return {
 //         movies: movies(state.movies,action),
@@ -60,5 +85,6 @@ const initialRootState = {
 
 export default combineReducers({
     movies,
-    search
-});
+    search,
+  });
+  
